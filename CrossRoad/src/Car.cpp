07dc4b672639c6e19e5a9c79a97c01c918382230
Ti Thong Thai <lhchau20@apcs.vn>
2021-12-01 17:18:48 +0700

    car.cpp finish

--- CrossRoad/src/Car.cpp
+++ CrossRoad/src/Car.cpp
@@ -1,14 +1,36 @@
 #include "../include/Car.h"
 
-Car::Car(int x, int y, bool isRight, float speed) : Vehicle(x, y, isRight, speed)
-{
-
+Car::Car(float x, float y, bool isRight, float speed) {
+	InitVariable(isRight);
+	InitTexture(isRight);
+	InitSprite(x, y);
 }
-void Car::DrawObject()
+void Car::InitTexture(bool isRight)
 {
-
+	if (isRight) {
+		if (!texture.loadFromFile("Sprite/Audi-right.png"))
+		{
+			cout << "Cannot find Sprite/Audi-right.png" << '\n';
+		}
+	}
+	else {
+		if (!texture.loadFromFile("Sprite/Audi-left.png"))
+		{
+			cout << "Cannot find Sprite/Audi-left.png" << '\n';
+		}
+	}
 }
-Car::~Car()
+void Car::InitSprite(float x, float y)
 {
+	sprite.setTexture(texture);
+	IntRect currentFrame = IntRect(0, 0, 256, 256);
+	sprite.setTextureRect(currentFrame);
+	sprite.setScale(0.5, 0.5);
+	sprite.setPosition(x, y);
+}
 
+bool Car::Intersect(FloatRect person)
+{
+	FloatRect rect = sprite.getGlobalBounds();
+	return rect.intersects(person);
 }
