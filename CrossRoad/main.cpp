062ff6e8a476e700ccc6230ce2b2dfde07723fbd
dknguyen176 <dknguyen176@gmail.com>
2021-12-02 11:37:21 +0700

    change main game loop

--- CrossRoad/main.cpp
+++ CrossRoad/main.cpp
@@ -4,17 +4,17 @@
 int main()
 {
     srand(static_cast<unsigned>(time(NULL))); 
-    Game game;
 
-    while (game.IsRunningGame())
-    {
-        game.Update();
+	for (int loop = 0; loop < 5; ++loop) {
+		Game game;
 
+		while (game.IsRunningGame())
+		{
+			game.Update();
 
-
-        game.Render();
-    }
-  
+			game.Render();
+		}
+	}
 
     
     return 0;
