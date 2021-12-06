c434ffb2f83332c1525b5a6c1360c0e3e45d43ad
dknguyen176 <dknguyen176@gmail.com>
2021-12-06 19:16:25 +0700

    add comment

--- CrossRoad/include/Game.h
+++ CrossRoad/include/Game.h
@@ -62,11 +62,11 @@ public:
 	virtual ~Game();
 
 	// main
-	void InitVariable(); // settings of game
+	void InitVariable(); // set some constant variable
 	void InitWindow(); // create window
 	void InitBackGround(); // load background + darken background image
 	void InitMusic(); // load and play background sound
-	void InitTextPoint(); // point text box
+	void InitTextPoint(); // create point text box
 	void InitMenu(); // create Menu (list of text box)
 
 	void PollingEvent(); // hotkeys / escape while playing
@@ -75,35 +75,34 @@ public:
 	void Render(); // draw game
 	void RenderMenu(); // draw menu
 
-	void InitLevel(); // init game (reset player + barrier + coin)
-	void CheckLevelUp(); // make change when level up
+	void InitLevel(); // init game level settings (reset player + barriers + coin)
+	void CheckLevelUp(); // change settings when level up (upgrade player + barriers, reset coin) 
 	void CheckColide(); // check colide between player and barriers
 
 	void LoadGame(); // load from text file or bin file or something (miss)
 
 	// player
-	void InitPlayer(); // set player poisition to the beginning
-	void UpgradePlayer();
+	void InitPlayer(); // init player position, speed
+	void UpgradePlayer(); // init player position, increase speed
 	void UpdatePlayer(); // update player animation + position
 	void RenderPlayer(); // draw player 
 
 	// barriers
-	void InitBarriers();
-	void UpgradeBarriers();
+	void InitBarriers(); // init barriers list, counter's range
+	void UpgradeBarriers(); // init barriers list, decrease counter's range
 	void UpdateBarriers(); // update barriers animation + position
-	bool visible(Barrier* barrier); // check if barrier is out of window
-	Barrier* GetBarrier(float x, float y, bool isRight, float speed); // get new barrier 
 	void RenderBarries(); // draw barriers
+	bool visible(Barrier* barrier); // check if barrier is out of window
+	Barrier* GetBarrier(float x, float y, bool isRight, float speed); // create new barrier 
 
 	// coin
-	void InitCoin(); // create coin at random position
+	void InitCoin(); // create coins at random positions
 	void UpdateCoin(); // update coins animation + position
 	void RenderCoin(); // draw coins
 
 	// point
-	void UpdateTextPoint(); // add point when collect coin
-	void RenderTextPoint(); // draw point text box
-
+	void UpdateTextPoint(); // update text point (+10 if player collects coin)
+	void RenderTextPoint(); // draw point text
 };
 
 #endif
