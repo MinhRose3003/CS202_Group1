98da68aff3a25fe78f48e22cd9cac5096e843a05
dknguyen176 <dknguyen176@gmail.com>
2021-12-07 15:23:18 +0700

    change coin spawn position

--- CrossRoad/src/GameCoin.cpp
+++ CrossRoad/src/GameCoin.cpp
@@ -10,9 +10,9 @@ void Game::InitCoin()
 	for (int i = 0; i < 10; i++)
 	{
 		int rX = rand() % ((width - 50) - 50 + 1) + 50;
-		int rY = rand() % ((height - 50) - 50 + 1) + 50;
+		int rY = rand() % 8;
 		float tmp1 = (float)rX;
-		float tmp2 = (float)rY;
+		float tmp2 = (float)line[rY];
 		Coin *coin = new Coin(tmp1, tmp2);
 		coinList.push_back(coin);
 	}
