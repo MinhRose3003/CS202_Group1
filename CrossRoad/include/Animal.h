c611251b34d824eb04c3f8aa64f24981df908fb7
dknguyen176 <dknguyen176@gmail.com>
2021-12-07 14:03:16 +0700

    comment unused code

--- CrossRoad/include/Animal.h
+++ CrossRoad/include/Animal.h
@@ -9,12 +9,12 @@ class Animal : public Barrier
 public:
 	virtual ~Animal() {}
 
-	virtual void InitImage(bool isRight) = 0;
-	virtual void InitTexture(bool isRight) = 0;
-	virtual void InitSprite(float x, float y) = 0;
+	//virtual void InitImage(bool isRight) = 0;
+	//virtual void InitTexture(bool isRight) = 0;
+	//virtual void InitSprite(float x, float y) = 0;
 
-	virtual Sprite GetHitbox() = 0;
-	virtual Image GetImage() = 0;
+	//virtual Sprite GetHitbox() = 0;
+	//virtual Image GetImage() = 0;
 };
 
 #endif
--- CrossRoad/include/Vehical.h
+++ CrossRoad/include/Vehical.h
@@ -9,12 +9,12 @@ class Vehicle : public Barrier
 public:
 	virtual ~Vehicle() {}
 
-	virtual void InitImage(bool isRight) = 0;
-	virtual void InitTexture(bool isRight) = 0;
-	virtual void InitSprite(float x, float y) = 0;
+	//virtual void InitImage(bool isRight) = 0;
+	//virtual void InitTexture(bool isRight) = 0;
+	//virtual void InitSprite(float x, float y) = 0;
 
-	virtual Sprite GetHitbox() = 0;
-	virtual Image GetImage() = 0;
+	//virtual Sprite GetHitbox() = 0;
+	//virtual Image GetImage() = 0;
 };
 
 #endif
